398eff1779a25b0e0b7548e7d067be73a11fd63b
= <=>
2020-12-24 16:43:20 +0700

    fix code

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'resources')));
 
 //Routes init
 route(app);
